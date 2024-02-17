6443972c12faab151551839d1073bfe1ce5dd70c
clementfavarel <plkode.dev@protonmail.com>
2024-02-17 19:02:18 +0100

    created first views

---
+++ src/view/welcome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bienvenue | MMISTIK</title>
+    <link rel="stylesheet" href="assets/css/global.css" />
+    <link rel="stylesheet" href="assets/css/welcome.css" />
+</head>
+
+<body>
+    <div class="card">
+        <h1>Bienvenue sur</h1>
+        <img class="logo" src="assets/img/logo-couleur.png" alt="MMISTIK" />
+        <h2 class="subtitle">Prêt à débuter ton aventure ?</h2>
+        <p>Alors inscris-toi et commence cette quête MMIstique !</p>
+        <a href="index.php?page=register" class="btn">Commencer</a>
+    </div>
+</body>
+
+</html>
