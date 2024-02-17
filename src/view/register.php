08b14e5e6e3861c541481f3480b2631114474250
clementfavarel <plkode.dev@protonmail.com>
2024-02-17 20:04:37 +0100

    register page

--- src/view/register.php
+++ src/view/register.php
@@ -1,1 +1,30 @@
-register
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Inscription | MMISTIK</title>
+    <link rel="stylesheet" href="assets/css/global.css" />
+    <link rel="stylesheet" href="assets/css/register.css" />
+</head>
+
+<body>
+    <div class="card">
+        <img src="assets/img/logo-couleur.png" alt="MMISTIK" class="logo" />
+        <h1>Décline ton identité</h1>
+        <form action="index.php/?page=register" method="post">
+            <div class="input-group">
+                <label for="username">Nom d'utilisateur</label>
+                <input type="text" name="username" id="username" required />
+            </div>
+            <div class="input-group">
+                <label for="email">Adresse email</label>
+                <input type="email" name="email" id="email" required />
+            </div>
+            <button type="submit" class="btn">S'inscrire</button>
+        </form>
+    </div>
+</body>
+
+</html>
